3022367e6155a40df2411a2e598b1bf58891f744
buitrison <61858817+buitrison@users.noreply.github.com>
2023-02-08 11:21:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ let infoForm = document.querySelector(".infoForm");
 let replaceEmail = document.querySelector(".replaceEmail");
 function checkEmail() {
   let email = document.getElementById("email");
-  const regex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([none-zA-Z\-0-9]+\.)+[none-zA-Z]{2,}))$/;
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
   console.log(email.value);
   if (!regex.test(email.value)) {
     alert("Dia chi email khong hop le.\nExample@gmail.com");
