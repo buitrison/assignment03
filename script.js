d2e7a8d74a7e2aa209406de98c99e5f59b8e3517
buitrison <61858817+buitrison@users.noreply.github.com>
2023-02-08 11:22:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function checkEmail() {
   let email = document.getElementById("email");
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  console.log(email.value);
+  
   if (!regex.test(email.value)) {
     alert("Dia chi email khong hop le.\nExample@gmail.com");
   } else {
