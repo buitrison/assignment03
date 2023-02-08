6bd0838fd0ebd80cb191117accafba59e780eb93
buitrison <61858817+buitrison@users.noreply.github.com>
2023-02-08 11:28:51 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # assignment03
+
 assignment 03 web funix bui tri son
 https://buitrison.github.io/assignment03/
--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let infoForm = document.querySelector(".infoForm");
 let replaceEmail = document.querySelector(".replaceEmail");
 function checkEmail() {
   let email = document.getElementById("email");
-  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  
   if (!regex.test(email.value)) {
     alert("Dia chi email khong hop le.\nExample@gmail.com");
   } else {
